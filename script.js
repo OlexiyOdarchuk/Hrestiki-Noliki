4565d4261bccc29f371c680f65f284dd77d57080
iShawyha <shawyhaf@gmail.com>
2023-09-14 10:05:27 +0300

    BRED

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ let boxes = document.querySelectorAll(".box");
 console.log(boxes);
 for(let i=0;i<boxes.length; i++){
     boxes[i].addEventListener("click",click)
-
 }
 
 function click() {
-    this.style.backgroundColor = "yellow"
+    boxes[5].classList.add("cross")
 }
