655b64836a5d0466b3bd72ed635b62841ef2af4e
iShawyha <shawyhaf@gmail.com>
2023-09-21 09:38:57 +0300

    Recode

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-let boxes = document.querySelectorAll(".box");
-console.log(boxes);
-for(let i=0;i<boxes.length; i++){
-    boxes[i].addEventListener("click",click)
-}
 
-let button = document.querySelector("button")
-button.setAttribute("disabled", "")
-
-function click() {
-    this.classList.remove("box")
-    this.classList.add("cross")
-    
+let boxes = document.querySelectorAll(".a")
+let played = []
+let k=1
+let pl = []
+for(let i = 0; i<boxes.length; i++){
+  
+  if(k%2!=0){
+    boxes[i].addEventListener("click", function() {
+      this.classList.remove("a")
+      this.classList.add("cross")
+      this.textContent = "❌"
+      played.push(this.id)
+    for(let j = 0; j<boxes.length-1; j++){
+      if (played.every(item => item != boxes[j])){
+        boxes[j].classList.remove("a")
+         boxes[j].classList.add("b")
+      }
+    }
+  });
+  }
+  else{
+    boxes[i].addEventListener("click", function() {
+      this.classList.remove("a")
+      this.classList.add("zero")
+      this.textContent = "🔵"
+  });
+  }
+  k+=1
 }
