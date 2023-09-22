16c635c9d7e35288c1875e89b55f345d9743b10a
iShawyha <shawyhaf@gmail.com>
2023-09-22 10:29:12 +0300

    Fix all

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-
+console.clear()
 let boxes = document.querySelectorAll(".a")
+console.log(boxes[0].id)
 let played = []
-let k=1
+let k = 1
 let pl = []
-for(let i = 0; i<boxes.length; i++){
-  
-  if(k%2!=0){
-    boxes[i].addEventListener("click", function() {
-      this.classList.remove("a")
-      this.classList.add("cross")
-      this.textContent = "❌"
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", function () {
       played.push(this.id)
-    for(let j = 0; j<boxes.length-1; j++){
-      if (played.every(item => item != boxes[j])){
-        boxes[j].classList.remove("a")
-         boxes[j].classList.add("b")
-      }
+      if (k % 2 != 0) {
+
+        for (let j = 0; j < boxes.length; j++) {
+                if (!played.some(item => item == boxes[j].id)) {
+                    boxes[j].classList.remove("a")
+                    boxes[j].classList.add("b")
+                }
+              else if (played.filter(item=>item == this.id).length == 1) {                                               this.classList.remove("a")
+            this.classList.add("cross")
+            this.textContent = "❌"
+              }
+            }
+        }
+        else {
+
+ 
+            for (let j = 0; j < boxes.length; j++) {
+                if (!played.some(item => item == boxes[j].id)) {
+                    boxes[j].classList.remove("b")
+                    boxes[j].classList.add("a")
+                }
+              else if (played.filter(item=>item == this.id).length == 1){
+            this.classList.remove("b")
+            this.classList.add("zero")
+            this.textContent = "🔵"
+              }
+            }
+
+        }
+        k ++
     }
-  });
-  }
-  else{
-    boxes[i].addEventListener("click", function() {
-      this.classList.remove("a")
-      this.classList.add("zero")
-      this.textContent = "🔵"
-  });
-  }
-  k+=1
+
+    );
+
 }
